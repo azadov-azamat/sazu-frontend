f896a9e9521ef5f1a6542fd1e7832eb9b881554c
azadov-azamat <azamatazadov112@gmail.com>
2024-07-29 20:36:07 +0500

    zoom image about section

--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -5,9 +5,14 @@ import {LazyLoadImage} from "react-lazy-load-image-component";
 import aboutImage from "../../assets/about-section.png";
 import Splitting from 'splitting';
 import {useEffect, useRef} from "react";
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 export default function Component() {
     const textRef = useRef<HTMLDivElement | null>(null);
+    const imageRef = useRef<HTMLDivElement | null>(null);
 
     useEffect(() => {
         Splitting();
@@ -49,24 +54,44 @@ export default function Component() {
         };
     }, []);
 
+    useEffect(() => {
+        if (imageRef.current) {
+            gsap.fromTo(
+                imageRef.current,
+                { scale: 1 },
+                {
+                    scale: 1.2,
+                    scrollTrigger: {
+                        trigger: imageRef.current,
+                        start: 'top bottom',
+                        end: 'bottom top',
+                        scrub: true,
+                    },
+                }
+            );
+        }
+    }, []);
 
     return (
         <section>
             <PageTitleComponent title={"О нас"}/>
             <div className={'flex md:flex-row flex-col gap-24 items-center mt-8'}>
+
                 <div
-                    data-aos="fade-up-right"
+                    ref={imageRef}
                     className={'md:w-[350px] w-3/4'}
                 >
                     <LazyLoadImage
-                        className={'w-full object-cover object-center'}
+                        className={'w-full object-cover object-center mediaFill parallaxImage revealStaggered'}
                         alt={"about-image"}
+                        style={{opacity: 0.9}}
                         src={aboutImage}
                     />
                 </div>
                 <p
                     ref={textRef}
-                    className="xl:w-[45%] w-full text-gray-500 font-normal md:text-3xl text-xl leading-9 words chars splitting" data-splitting>
+                    className="xl:w-[45%] w-full text-gray-500 font-normal md:text-3xl text-xl leading-9 words chars splitting"
+                    data-splitting>
                     Sazu - это команда опытных специалистов в сфере маркетинга, которые специализируются
                     на продвижении жилых комплексов. Мы обладаем глубоким пониманием рынка
                     недвижимости и используем инновационные методы, чтобы создавать эффективные
