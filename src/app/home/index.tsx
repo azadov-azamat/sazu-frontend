a604ce7194e2f014b0f7057eb9930042096645cb
azadov-azamat <azamatazadov112@gmail.com>
2024-07-29 16:23:22 +0500

    edit gap

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -1,11 +1,12 @@
 // importrt React, {useEffect} from 'react';
 
-import {AboutSection, HeaderSection, NewsSection, PartnersSection} from "../../components";
+import {AboutSection, HeaderSection, NewsSection, PartnersSection, SiteLoadingComponent} from "../../components";
 
 export default function Controller() {
 
     return (
-        <div className={'flex flex-col gap-14'}>
+        <div className={'flex flex-col gap-24'}>
+            <SiteLoadingComponent/>
             <HeaderSection/>
             <AboutSection/>
             <PartnersSection/>
