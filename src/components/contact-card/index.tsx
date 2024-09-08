1dc55e952f60d3e0e7448f2f007ec7466f5ecf23
azadov-azamat <azamatazadov112@gmail.com>
2024-09-08 14:36:06 +0500

    card

--- src/components/contact-card/index.tsx
+++ src/components/contact-card/index.tsx
@@ -2,19 +2,19 @@
 
 import {contactDataKey} from "../../interface/redux/variable.interface.ts";
 // import {useTranslation} from "react-i18next";
-import {LazyLoadImage} from "react-lazy-load-image-component";
+// import {LazyLoadImage} from "react-lazy-load-image-component";
 
 export default function Component({name, image, profession, text}: contactDataKey) {
     // const {t} = useTranslation()
 
     return (
-        <div className="relative w-full xl:h-[490px] md:h-[360px] h-80 bg-black rounded-xl overflow-hidden shadow-lg group">
-            <LazyLoadImage
-                effect="blur"
-                rel={'preload'}
+        <div className="relative w-full xl:h-[440px] md:h-[360px] h-80 bg-black rounded-xl overflow-hidden shadow-lg group">
+            <img
                 src={image}
                 alt={name}
-                className="w-full h-full object-cover object-top transition duration-500 group-hover:blur-sm ease-in-out group-hover:scale-95"
+                loading={'lazy'}
+                className="!w-full !h-full transition duration-500 object-cover md:object-center object-top
+                group-hover:blur-sm ease-in-out group-hover:scale-95"
             />
             <div
                 className="absolute inset-0 flex flex-col justify-end p-4 transition-all duration-500 bg-gradient-to-t from-black
