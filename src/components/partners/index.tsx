8b11931cea855c7ec5226ba1e5b1ddf849ace0d7
azadov-azamat <azamatazadov112@gmail.com>
2024-10-23 20:34:33 +0500

    fix

--- src/components/partners/index.tsx
+++ src/components/partners/index.tsx
@@ -12,28 +12,10 @@ export default function Component() {
         <div
             id={'partners'}
             className="flex flex-col items-center justify-center text-center">
-            <div className="flex w-full overflow-hidden">
-                <Marquee speed={80} gradient={false}>
+            <div className="flex w-full h-auto">
+                <Marquee speed={80} gradient={false} className={'overflow-y-hidden'}>
                     <div
-                        className="flex md:gap-32 sm:gap-24 gap-14 select-none md:mr-32 sm:!mr-24 !mr-14">
-                        {partners.map((logo, index) => (
-                            <div key={index} className={'w-32 h-14 flex-shrink-0'}>
-                                <LazyLoadImage src={logo.icon} alt={logo.icon}
-                                               className="w-full h-full mx-4 object-contain"/>
-                            </div>
-                        ))}
-                    </div>
-                    <div
-                        className="flex md:gap-32 sm:gap-24 gap-14 select-none md:mr-32 sm:!mr-24 !mr-14">
-                        {partners.map((logo, index) => (
-                            <div key={index} className={'w-32 h-14 flex-shrink-0'}>
-                                <LazyLoadImage src={logo.icon} alt={logo.icon}
-                                               className="w-full h-full mx-4 object-contain"/>
-                            </div>
-                        ))}
-                    </div>
-                    <div
-                        className="flex md:gap-32 sm:gap-24 gap-14 select-none md:mr-32 sm:!mr-24 !mr-14">
+                        className="flex w-full h-auto md:gap-32 sm:gap-24 gap-14 select-none md:mr-32 sm:!mr-24 !mr-14">
                         {partners.map((logo, index) => (
                             <div key={index} className={'w-32 h-14 flex-shrink-0'}>
                                 <LazyLoadImage src={logo.icon} alt={logo.icon}
