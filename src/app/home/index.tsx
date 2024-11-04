3b24c85f0ab44fc2e8c7d413887bde3ab514ba1f
azadov-azamat <azamatazadov112@gmail.com>
2024-11-04 12:11:26 +0500

    performance

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -29,7 +29,7 @@ export default function Controller() {
     return (
         <div className={'flex flex-col md:gap-32 sm:gap-20 gap-14'}>
             <HeaderSection/>
-            <div className="flex flex-col gap-4 items-center h-auto">
+            <div className="flex flex-col gap-4 items-center min-h-[400px]">
                 {
                     projects.map((project, index) => (
                         <ProjectCardComponent {...project} index={index} total={projects.length} key={index}/>
--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,44 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
-import { config } from 'dotenv';
+import {config} from 'dotenv';
 import svgr from '@svgr/rollup'
+import viteCompression from 'vite-plugin-compression';
 
 config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
-  define: {
-    'process.env': process.env
-  },
-  optimizeDeps: {
-    include: ['framer-motion', 'react-fast-marquee'],
-  },
-  server: {
-    watch: {
-      usePolling: true,
+    cacheDir: './.vite_cache',
+    plugins: [
+        react(),
+        svgr(),
+        viteCompression({
+            algorithm: 'gzip', // 'gzip' / 'brotliCompress'
+            threshold: 10240, // 10KB dan katta fayllarni siqish
+        }),
+    ],
+    define: {
+        'process.env': process.env
     },
-  },
+    optimizeDeps: {
+        include: ['framer-motion', 'react-fast-marquee'],
+    },
+    server: {
+        watch: {
+            usePolling: true,
+        },
+    },
+    build: {
+        target: 'esnext', // Zamonaviy brauzerlar uchun optimallashtirilgan kod yaratish
+        sourcemap: false, // Sourcemap'ni o'chirish yuklanishni tezlashtiradi
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                    }
+                },
+            },
+        },
+    },
 })
